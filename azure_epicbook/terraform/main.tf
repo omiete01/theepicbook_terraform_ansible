2737c26c461e507db75ce97dbf1dce553373ea33
Esther Fyneface <105213543+omiete01@users.noreply.github.com>
2025-12-01 10:04:43 +0100

    Upgrade azurerm provider version to 4.1.0

--- azure_epicbook/terraform/main.tf
+++ azure_epicbook/terraform/main.tf
@@ -2,14 +2,14 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "=3.0.0"
+      version = "=4.1.0"
     }
   }
 }
 
 provider "azurerm" {
   features {}
-  subscription_id = "048a1739-6bf8-4f17-acf5-263808ba8d22"
+  # subscription_id = "048a1739-6bf8-4f17-acf5-263808ba8d22"
 }
 
 # Azure Resource Group
